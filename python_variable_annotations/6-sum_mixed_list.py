6ad3d63be69c69a28abc305084015c63003719ee
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-14 15:02:25 +1100

    add 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Add two numbers"""
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """Sum numbers"""
+    return sum(mxd_lst)
