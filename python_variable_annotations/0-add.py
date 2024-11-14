8c040365453bea95fb447e3557d48c775e343ab7
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-14 14:39:10 +1100

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""Queries the Reddit API and returns the titles of all hot articles for a given subreddit"""
+"""Add two numbers"""
 
-    
+
 def add(a: float, b: float) -> float:
     """Add two numbers"""
     return a + b
