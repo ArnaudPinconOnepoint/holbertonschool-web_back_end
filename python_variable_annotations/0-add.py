ebaf123848bc8c07a8a1f5c38337fe377534aca1
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-14 14:34:48 +1100

    ADD 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and returns the titles of all hot articles for a given subreddit"""
+
+    
+def add(a, b):
+    """Add two numbers"""
+    return a + b
