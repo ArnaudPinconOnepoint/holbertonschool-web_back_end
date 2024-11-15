1844df21714079cd244017862b5249f0ce49d67e
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-15 13:15:47 +1100

    fix 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -14,9 +14,10 @@ def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
-    
+
     total_time = end_time - start_time
     return total_time / n
+
 
 """ if __name__ == "__main__":
     n = 5
