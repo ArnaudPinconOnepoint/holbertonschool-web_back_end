b6b10b024235a369ac28abf2d152a56347143aa4
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-15 13:20:18 +1100

    add 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -4,21 +4,22 @@
 import asyncio
 import importlib
 import heapq
+from typing import List
 
 tasks = importlib.import_module("3-tasks")
 task_wait_random = tasks.task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
-    """Spawn wait_random n times with max_delay and return delays in ascending order."""
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times with max_delay"""
     delays = []
     # Spawn `wait_random` n times and gather results
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
         delay = await task
-        heapq.heappush(delays, delay)  # Use heapq to keep delays in order
+        heapq.heappush(delays, delay)
 
-    return [heapq.heappop(delays) for _ in range(n)]  # Pop in ascending order
+    return [heapq.heappop(delays) for _ in range(n)]
 
 # if __name__ == "__main__":
 #     n = 5
