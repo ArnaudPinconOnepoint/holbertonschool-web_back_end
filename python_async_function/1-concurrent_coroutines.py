bf80c3c2597c6f2dcb6f19733650d8b23a98d2dd
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-15 11:15:20 +1100

    add 2

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -15,7 +15,6 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     tasks = [wait_random(max_delay) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
         delay = await task
-        print(delay)
         heapq.heappush(delays, delay)  # Use heapq to keep delays in order
 
     return [heapq.heappop(delays) for _ in range(n)]  # Pop in ascending order
---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import asyncio
+import importlib
+import time
+
+concurrent_coroutines = importlib.import_module("1-concurrent_coroutines")
+wait_n = concurrent_coroutines.wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure the total execution time for wait_n(n, max_delay) and return total_time / n."""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    
+    total_time = end_time - start_time
+    return total_time / n
+
+""" if __name__ == "__main__":
+    n = 5
+    max_delay = 10
+    print(measure_time(n, max_delay)) """
