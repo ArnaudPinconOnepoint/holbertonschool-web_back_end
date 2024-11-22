ce64f165a107ed443f0e0ac1196fabbdde73b78c
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-22 11:21:12 +1100

    add 0 fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ async def async_generator():
     for _ in range(10):
         await asyncio.sleep(1)  # Asynchronously wait for 1 second
         yield random.uniform(0, 10)  # Yield a random float between 0 and 10
-
+        
 """ async def main():
     async for number in async_generator():
         print(number)
