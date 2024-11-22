afdfca4fa53e03b4d98da9023dd0bfecca0ca935
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-22 11:35:19 +1100

    fix pycode

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,6 +4,7 @@
 import asyncio
 import random
 
+
 async def async_generator():
     """Yield 10 random floats between 0 and 10"""
     for _ in range(10):
--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,6 +7,7 @@ from typing import List
 basic_async_syntax = importlib.import_module("0-async_generator")
 async_generator = basic_async_syntax.async_generator
 
+
 # The async_comprehension coroutine
 async def async_comprehension() -> List[float]:
     """Collects 10 random numbers using an async comprehension."""
