eff247486867fefa6dead757a90b41d306194075
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-25 15:57:57 +1100

    fix

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,8 +1,12 @@
-#!/usr/bin/env python3
 from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
+    """
+    Lists all documents in a specified MongoDB collection.
+    """
     # Find all documents in the collection and return them as a list
     documents = list(mongo_collection.find())
+
+    # Return the list of documents (empty if none found)
     return documents
