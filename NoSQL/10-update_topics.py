43a40b35273ba4235916e9b1af51b3dc6d434f86
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-25 16:18:26 +1100

    add 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Documented"""
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Lists all documents in a specified MongoDB collection.
+    """
+    # Insert the document into the collection
+    mongo_collection.updateMany({"name": name}, {"$set": {"topics": topics}})
