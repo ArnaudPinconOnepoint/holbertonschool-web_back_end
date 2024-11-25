fbe306320c51ca1eca39cd738440a2e3ae2109e1
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-25 16:19:47 +1100

    fix

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -9,4 +9,4 @@ def update_topics(mongo_collection, name, topics):
     Lists all documents in a specified MongoDB collection.
     """
     # Insert the document into the collection
-    mongo_collection.updateMany({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
