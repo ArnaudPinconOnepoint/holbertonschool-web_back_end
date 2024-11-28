872360a51e3adf6f89d2064e67cd50beb8317c62
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-28 15:00:49 +1100

    add 3

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import Dict, List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+    
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Get page """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start_index, end_index = self.index_range(page, page_size)
+        dataset = self.dataset()
+        return dataset[start_index:end_index]
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        assert index >= 0 and index < len(self.dataset())
+        
+        page = index // page_size
+        reste = index % page_size
+
+        rangeIndex = self.get_page(page, page_size)
+        new_index = rangeIndex[0]
+        next_index = rangeIndex[1]+1 if rangeIndex[1] < len(self.dataset()) - 1 else None
+
+        result = {
+            "index": new_index,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": self.dataset()[new_index:next_index-1],
+        }
+
+        return result
