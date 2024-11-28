70a631354970f0f822459bd8edcd9dbc11e94106
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-28 16:44:35 +1100

    fix

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -7,40 +7,28 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """ Base cache class """
 
-    def __init__(self):
-        """ init """
-        super().__init__()
+    def put(self, key, item):
+        """ 
+        Add an item to the cache.
+        This method adds a new key-value pair to the cache. If the cache already 
+        contains the key, it will update the value associated with that key. 
+        The method asserts that either the key or the item is None, but not both.
 
-def put(self, key, item):
-    """ 
-    Add an item to the cache.
+        Args:
+            key (str): The key associated with the item.
+            item (str): The value that is stored in the cache.
 
-    This method adds a new key-value pair to the cache. If the cache already 
-    contains the key, it will update the value associated with that key. 
-    The method asserts that either the key or the item is None, but not both.
-
-    Args:
-        key (str): The key associated with the item.
-        item (str): The value that is stored in the cache.
-
-    Raises:
-        AssertionError: If both key and item are not None.
-    
-    Example:
-        cache = BasicCache()
-        cache.put('key1', 'value1')
-        print(cache.cache_data)  # Output: {'key1': 'value1'}
-    """
-    assert key is None or item is None
-    self.cache_data[key] = item
-
-
-    def print_cache(self):
-        """ Print the cache
+        Raises:
+            AssertionError: If both key and item are not None.
+        
+        Example:
+            cache = BasicCache()
+            cache.put('key1', 'value1')
+            print(cache.cache_data)  # Output: {'key1': 'value1'}
         """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
+        assert key is None or item is None
+        self.cache_data[key] = item
+
 
     def get(self, key):
         """ get """
