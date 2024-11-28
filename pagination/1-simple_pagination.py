4e76e6b00aa07431404184c271c6822d227f0f94
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-28 11:37:36 +1100

    fix 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -33,8 +33,9 @@ class Server:
         dataset = self.dataset()
         return dataset[start_index:end_index]
 
+
 def index_range(page: int, page_size: int) -> tuple[int, int]:
-    """Return a tuple containing the start and end indexes for a page."""
+    """Return a tuple"""
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
