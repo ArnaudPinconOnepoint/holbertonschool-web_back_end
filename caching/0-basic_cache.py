75015f87d0dc85f0241ed1c90c0219bad4e6eac5
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-28 15:40:39 +1100

    add 0

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+
+class BaseCaching():
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        raise NotImplementedError("put must be implemented in your cache class")
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        raise NotImplementedError("get must be implemented in your cache class")
+    
+
+class BaseCache(BaseCaching):
+    """ Base cache class """
+    def put(self, key, item):
+        assert key is None or item is None
+        self.cache_data[key] = item
+
+    def get(self, key):
+        assert key is None or self.cache_data[key] is None
+        return self.cache_data[key]
