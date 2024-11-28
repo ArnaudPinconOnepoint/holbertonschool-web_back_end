4b23ca74b15f58c6ef81a677cf4ed652ee2aad69
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-28 15:45:09 +1100

    fix

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -36,12 +36,15 @@ class BasicCache(BaseCaching):
     """ Base cache class """
 
     def __init__(self):
+        """ init """
         super().__init__()
 
     def put(self, key, item):
+        """ put """
         assert key is None or item is None
         self.cache_data[key] = item
 
     def get(self, key):
+        """ get """
         assert key is None or self.cache_data[key] is None
         return self.cache_data[key]
