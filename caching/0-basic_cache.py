95a5cd265afaa6ce46b6ef37acaba548c4716026
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-28 16:41:23 +1100

    doc

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -11,10 +11,28 @@ class BasicCache(BaseCaching):
         """ init """
         super().__init__()
 
-    def put(self, key, item):
-        """ put """
-        assert key is None or item is None
-        self.cache_data[key] = item
+def put(self, key, item):
+    """ 
+    Add an item to the cache.
+
+    This method adds a new key-value pair to the cache. If the cache already 
+    contains the key, it will update the value associated with that key. 
+    The method asserts that either the key or the item is None, but not both.
+
+    Args:
+        key (str): The key associated with the item.
+        item (str): The value that is stored in the cache.
+
+    Raises:
+        AssertionError: If both key and item are not None.
+    
+    Example:
+        cache = BasicCache()
+        cache.put('key1', 'value1')
+        print(cache.cache_data)  # Output: {'key1': 'value1'}
+    """
+    assert key is None or item is None
+    self.cache_data[key] = item
 
     def print_cache(self):
         """ Print the cache
