6db4440a487e955ea3189705c3022179e5c7b3aa
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-28 11:03:37 +1100

    fix 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,5 +3,7 @@
 
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
-    """floor a number"""
-    return (page, page_size)
+    """Return a tuple containing the start and end indexes for a page."""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
