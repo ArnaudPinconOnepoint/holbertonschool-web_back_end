ccf8afbbc32d8956b511b472382815195543dab7
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-28 16:52:54 +1100

    fix

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -8,7 +8,7 @@ class BasicCache(BaseCaching):
     """ Base cache class """
 
     def put(self, key, item):
-        """ 
+        """
         Add an item to the cache.
         """
         if key is None or item is None:
@@ -17,7 +17,7 @@ class BasicCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        """ 
+        """
         Get a item with a key
         """
         if key is None or key not in self.cache_data:
