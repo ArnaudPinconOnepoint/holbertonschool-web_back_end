36b22933a16a5d95c9d0998da2daf9694042aeed
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-11-28 16:40:23 +1100

    try

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -16,6 +16,13 @@ class BasicCache(BaseCaching):
         assert key is None or item is None
         self.cache_data[key] = item
 
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
     def get(self, key):
         """ get """
         assert key is None or self.cache_data[key] is None
