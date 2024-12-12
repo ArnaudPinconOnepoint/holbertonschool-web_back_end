df5138d1fa4c336862041b6b0486e6be3764c50d
Arnaud Pinçon <a.pincon@groupeonepoint.com>
2024-12-12 13:18:36 +1100

    add 3

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -2,8 +2,8 @@
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
-    """LIFO caching system."""
+class LRUCache(BaseCaching):
+    """LRU caching system."""
 
     def __init__(self):
         """Initialize the class."""
@@ -12,9 +12,9 @@ class LIFOCache(BaseCaching):
 
     def put(self, key, item):
         """
-        Add an item in the cache using LIFO algorithm.
+        Add an item in the cache using LRU algorithm.
 
-        If the cache exceeds MAX_ITEMS, remove the last item added.
+        If the cache exceeds MAX_ITEMS, remove the Least Recently Used item.
         """
         if key is None or item is None:
             return
@@ -28,9 +28,9 @@ class LIFOCache(BaseCaching):
         self.cache_data[key] = item
         self.keys_order.append(key)
 
-        # If the cache exceeds MAX_ITEMS, evict the last added key
+        # If the cache exceeds MAX_ITEMS, evict the Least Recently Used
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key = self.keys_order.pop(-2)
+            last_key = self.keys_order.pop(-1)
             del self.cache_data[last_key]
             print(f"DISCARD: {last_key}")
 
@@ -40,4 +40,8 @@ class LIFOCache(BaseCaching):
 
         Return None if the key is None or does not exist in the cache.
         """
+        value = self.cache_data.get(key, None)
+        if value is not None:
+            self.keys_order.remove(key)
+            self.keys_order.insert(0, key)
         return self.cache_data.get(key, None)
